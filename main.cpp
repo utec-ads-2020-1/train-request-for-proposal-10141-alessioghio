6af412f3edd7c71dc31e5d4162c5fdb0771cffdc
Alessio <alessiogdanz@gmail.com>
2020-03-30 18:42:21 -0500

    Reduced number of times find function is being called

--- main.cpp
+++ main.cpp
@@ -24,36 +24,37 @@ int main(){
         // line containing two integers: 0 < n <= 1000
         // n = the number of requirements
         // p = number of proposals
-        bool strHasPeriod = find(inputLine.begin(), inputLine.end(),'.') != inputLine.end();
-        if( !(strHasPeriod) && isdigit(inputLine.c_str()[0])){
-            stringstream ss(inputLine);
-            ss >> n >> p;
-            if (n==0 && p == 0){ // The line 0 0 indicates there are no more RFPs
-                cout << "RFP #" << numRFP << endl << bestProp << endl;
-                break;
-            }
-            if (numRFP!=0){
-                cout << "RFP #" << numRFP << endl << bestProp << endl << endl;
-            }
-            numRFP++;
-            maxComp = 0;
-            minPrice = numeric_limits<float>::max();
-            ss.clear();
-            continue;
-        }
-        if(strHasPeriod){
-            d = stof(inputLine, &sz);
-            r = stoi(inputLine.substr(sz));
-            compliance = (float)r/(float)n;
-            if (maxComp == compliance && minPrice > d){
-                bestProp = lastInput;
-                minPrice = d;
-            }else if(maxComp < compliance){
-                bestProp = lastInput;
-                maxComp = compliance;
-                minPrice = d;           
-            }    
-        }  
+        if(isdigit(inputLine.c_str()[0])){
+            bool strHasPeriod = find(inputLine.begin(), inputLine.end(),'.') != inputLine.end();
+            if (!(strHasPeriod)){
+                stringstream ss(inputLine);
+                ss >> n >> p;
+                if (n==0 && p == 0){ // The line 0 0 indicates there are no more RFPs
+                    cout << "RFP #" << numRFP << endl << bestProp << endl;
+                    break;
+                }
+                if (numRFP!=0){
+                    cout << "RFP #" << numRFP << endl << bestProp << endl << endl;
+                }
+                numRFP++;
+                maxComp = 0;
+                minPrice = numeric_limits<float>::max();
+                ss.clear();
+                continue;
+            }else{
+                d = stof(inputLine, &sz);
+                r = stoi(inputLine.substr(sz));
+                compliance = (float)r/(float)n;
+                if (maxComp == compliance && minPrice > d){
+                    bestProp = lastInput;
+                    minPrice = d;
+                }else if(maxComp < compliance){
+                    bestProp = lastInput;
+                    maxComp = compliance;
+                    minPrice = d;           
+                }
+            }            
+        }     
         lastInput = inputLine;
     }
     return EXIT_SUCCESS;
