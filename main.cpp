9f00498365bb6eed77819b155d9b0e6a66e9f097
Alessio <alessiogdanz@gmail.com>
2020-03-30 01:01:34 -0500

    imrpoved performance

--- main.cpp
+++ main.cpp
@@ -11,17 +11,17 @@ int main(){
     string inputLine;
     string lastInput;
     string::size_type sz;
-    vector<pair<int, string>> result;
+    string bestProp;
     int numRFP = 0;
     int p, n;
     float d;
     int r;
-    float compliance = 0;
+    float compliance;
     float maxComp;
     float minPrice;
-    std::ifstream file("input.txt");
+    //fstream file("input.txt");
 
-    while(getline(file, inputLine)){
+    while(getline(cin, inputLine)){
         // line containing two integers: 0 < n <= 1000
         // n = the number of requirements
         // p = number of proposals
@@ -30,10 +30,13 @@ int main(){
             stringstream ss(inputLine);
             ss >> n >> p;
             if (n==0 && p == 0){ // The line 0 0 indicates there are no more RFPs
+                cout << "RFP #" << numRFP << endl << bestProp << endl;
                 break;
             }
+            if (numRFP!=0){
+                cout << "RFP #" << numRFP << endl << bestProp << endl << endl;
+            }
             numRFP++;
-            result.push_back(make_pair(numRFP,""));
             maxComp = 0;
             minPrice = numeric_limits<float>::max();
             ss.clear();
@@ -44,24 +47,16 @@ int main(){
             r = stoi(inputLine.substr(sz));
             compliance = (float)r/(float)n;
             if (maxComp == compliance && minPrice > d){
-                result[numRFP-1].second = lastInput;
+                bestProp = lastInput;
                 minPrice = d;
             }else if(maxComp < compliance){
-                result[numRFP-1].second = lastInput;
+                bestProp = lastInput;
                 maxComp = compliance;
                 minPrice = d;           
-            }
-            
+            }    
         }  
         lastInput = inputLine;
     }
-    int size = result.size()-1;
-    for (int i = 0; i < size; i++){
-        cout << "RFP #" << result[i].first << endl;
-        cout << result[i].second << endl << endl;
-    }
-    cout << "RFP #" << result[size].first << endl;
-    cout << result[size].second << endl;
     
     return EXIT_SUCCESS;
 }
